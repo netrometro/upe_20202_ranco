4fe5e9fc484bdf89c07506d40c335c9b13059709
DahiseDias <dahisedias@gmail.com>
2021-07-20 11:48:12 -0300

    criação classe abstrata usuario

---
+++ src/main/java/models/Usuario.java
@@ -0,0 +1,36 @@
+package models;
+
+public abstract class Usuario {
+	
+	private String nome;
+	private String email;
+	private String senha;
+	private int tipoUsuario;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public int getTipoUsuario() {
+		return tipoUsuario;
+	}
+	public void setTipoUsuario(int tipoUsuario) {
+		this.tipoUsuario = tipoUsuario;
+	}
+	
+	
+
+}
