29576ee7a4aa187a361158c7e3209739e0ceb9ce
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-07-27 10:30:53 -0300

    MedicacaoService, findAll and findbyid implemented

--- src/main/java/application/services/MedicacaoService.java
+++ src/main/java/application/services/MedicacaoService.java
@@ -1,5 +1,30 @@
 package application.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import application.models.Medicacao;
+import application.repositories.MedicacaoRepository;
+
+@Service
 public class MedicacaoService {
 
+	@Autowired
+	private MedicacaoRepository medicacoes;
+	
+	public ResponseEntity<List<Medicacao>> findAll(){
+		return ResponseEntity.ok(medicacoes.findAll());
+	}
+	
+	public ResponseEntity<Medicacao> findById(Long medicacao_id){		
+		Optional<Medicacao> medicacao = medicacoes.findById(medicacao_id);
+		if(!medicacao.isEmpty()) {
+			ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(medicacao.get());
+	}
 }
