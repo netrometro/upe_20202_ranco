2dc5e822ebc5b7b63ef96663955ce98049accacf
DahiseDias <dahisedias@gmail.com>
2021-07-27 10:42:02 -0300

    paciente service get

--- src/main/java/application/services/PacienteService.java
+++ src/main/java/application/services/PacienteService.java
@@ -1,15 +1,33 @@
 package application.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import application.models.Paciente;
 import application.repositories.PacienteRepository;
 
 @Service
 public class PacienteService {
 	
 	@Autowired
-	private PacienteRepository pacienteDAO;
+	private PacienteRepository pacientes;
 	
+	public ResponseEntity<List<Paciente>> findAll(){
+		return ResponseEntity.ok(pacientes.findAll());
+	}
+	
+	public ResponseEntity<Paciente> findById(Long id_paciente){
+		Optional<Paciente> paciente = pacientes.findById(id_paciente);
+		if(paciente.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		else {
+			return ResponseEntity.ok(paciente.get());
+		}
+	}
 	
 }
