0910989e626037670a45e0ff98e8e704d6a1128f
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-07-27 10:21:32 -0300

    Correction in psicologo repository

--- src/main/java/application/repositories/PsicologoRepository.java
+++ src/main/java/application/repositories/PsicologoRepository.java
@@ -5,12 +5,12 @@ package application.repositories;
 
 import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import application.models.Psicologo;
 
 
-public interface PsicologoRepository extends CrudRepository<Psicologo, Long> {
+public interface PsicologoRepository extends JpaRepository<Psicologo, Long> {
 	Optional<Psicologo> findBycrp(String crp);
 	
 }
