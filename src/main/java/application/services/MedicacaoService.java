febf4857d43600e75cc0838f8c3e92710122bf7a
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-07-27 10:40:26 -0300

    Implementind create method

--- src/main/java/application/services/MedicacaoService.java
+++ src/main/java/application/services/MedicacaoService.java
@@ -8,7 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import application.models.Medicacao;
+import application.models.Paciente;
 import application.repositories.MedicacaoRepository;
+import application.repositories.PacienteRepository;
 
 @Service
 public class MedicacaoService {
@@ -16,15 +18,18 @@ public class MedicacaoService {
 	@Autowired
 	private MedicacaoRepository medicacoes;
 	
-	public ResponseEntity<List<Medicacao>> findAll(){
+	@Autowired
+	private PacienteRepository pacientes;
+
+	public ResponseEntity<List<Medicacao>> findAll() {
 		return ResponseEntity.ok(medicacoes.findAll());
 	}
-	
-	public ResponseEntity<Medicacao> findById(Long medicacao_id){		
+
+	public ResponseEntity<Medicacao> findById(Long medicacao_id) {
 		Optional<Medicacao> medicacao = medicacoes.findById(medicacao_id);
-		if(!medicacao.isEmpty()) {
+		if (!medicacao.isEmpty()) {
 			ResponseEntity.notFound().build();
 		}
 		return ResponseEntity.ok(medicacao.get());
-	}
+	}	
 }
