e180e94fc80d5812d763998724ce0ced7132866f
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-07-29 11:47:12 -0300

    Small correction on eventos CRUD

--- src/main/java/application/models/Evento.java
+++ src/main/java/application/models/Evento.java
@@ -12,61 +12,43 @@ import lombok.Data;
 @Data
 @Entity
 public class Evento {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id_evento;
-	private Date data; 
-	private String local; 
-	private String pessoaEnvolvida; 
+	private Date data;
+	private String local;
+	private String pessoaEnvolvida;
 	private String testemunhas;
 	private String expectativa;
 	private String descricao;
-	private int categoria; 
-	private String motivo; 
-	private String prova; 
+	private int categoria;
+	private String motivo;
+	private String prova;
 	private String pontoMelhoria;
 	private String solucao;
 	private String feedback;
 	private boolean status;
-	private String recomendacao; 
+	private String recomendacao;
 	private String sentimentos;
-	
-	@ManyToOne	
-	private Paciente usuario; 
-	
-	public Evento(Date data, String local, String pessoaEnvolvida, String testemunhas, String expectativa, String descricao, int categoria, 
-			String motivo, String prova, String pontoMelhoria, String solucao, String feedback, boolean status, String recomendacao, String sentimentos){
-		super();
-		this.data = data;
-		this.local = local;
-		this.pessoaEnvolvida = pessoaEnvolvida;
-		this.testemunhas = testemunhas;
-		this.expectativa = expectativa;
-		this.descricao = descricao; 
-		this.categoria = categoria; 
-		this.motivo = motivo;
-		this.prova = prova;
-		this.pontoMelhoria = pontoMelhoria;
-		this.solucao = solucao;
-		this.feedback = feedback;
-		this.status = status;
-		this.recomendacao = recomendacao;
-		this.sentimentos = sentimentos; 
-		
-		
+
+	@ManyToOne
+	private Paciente usuario;
+
+	public Evento() {
 	}
-	
-	public Evento(Date data, String local, String pessoaEnvolvida, String testemunhas, String expectativa, String descricao, int categoria, 
-			String motivo, String prova, String pontoMelhoria, String solucao, String feedback, boolean status, String recomendacao, String sentimentos, Paciente usuario){
+
+	public Evento(Date data, String local, String pessoaEnvolvida, String testemunhas, String expectativa,
+			String descricao, int categoria, String motivo, String prova, String pontoMelhoria, String solucao,
+			String feedback, boolean status, String recomendacao, String sentimentos, Paciente usuario) {
 		super();
 		this.data = data;
 		this.local = local;
 		this.pessoaEnvolvida = pessoaEnvolvida;
 		this.testemunhas = testemunhas;
 		this.expectativa = expectativa;
-		this.descricao = descricao; 
-		this.categoria = categoria; 
+		this.descricao = descricao;
+		this.categoria = categoria;
 		this.motivo = motivo;
 		this.prova = prova;
 		this.pontoMelhoria = pontoMelhoria;
@@ -76,9 +58,6 @@ public class Evento {
 		this.recomendacao = recomendacao;
 		this.sentimentos = sentimentos;
 		this.usuario = usuario;
-		
-		
 	}
 
-
-} 
+}
--- src/main/java/application/repositories/EventoRepository.java
+++ src/main/java/application/repositories/EventoRepository.java
@@ -1,12 +1,9 @@
 package application.repositories;
 
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import application.models.Evento;
 
-public interface EventoRepository extends JpaRepository<Evento, Long>{
-	Optional<Evento> findByNome(String id_evento);
-
+public interface EventoRepository extends JpaRepository<Evento, Long>{	
 }
