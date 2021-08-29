b5808fa6b9bd096ff039dbd3af9e492815cbecb4
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-08-29 17:36:06 -0300

    Included navigation system

--- src/main/rancoapp/src/App.js
+++ src/main/rancoapp/src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import EventoDetalhes from './view/EventoDetalhes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>INVOCADO</h1>
-      </header>
-    </div>
+    <Router>
+      {/* <Navigation /> */}
+      <Switch>
+        <Route path='/' exact component={EventoDetalhes} ></Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/main/rancoapp/src/view/EventoDetalhes.js
+++ src/main/rancoapp/src/view/EventoDetalhes.js
@@ -1,2 +1,12 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+
+class EventoDetalhes extends React.Component {
+    render() {
+        return (
+            <h1>Detalhes de evento</h1>
+        );
+    }
+}
+
+export default EventoDetalhes;
