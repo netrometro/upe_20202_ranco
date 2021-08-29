061d9f79912e7c532d7deefc5f065c902fcf45f6
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-08-29 20:59:11 -0300

    Getting eventos list working

--- src/main/rancoapp/src/view/EventoDetalhes.js
+++ src/main/rancoapp/src/view/EventoDetalhes.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from 'react-dom';
 
-export default () => {
+export default ({match}) => {
     useEffect(() => {
-        getEvento();
+        getEvento(match.id);
     }, [])
 
     const [evento, setEvento] = useState({});
 
-    const getEvento = () => {
-        fetch('http://localhost:5000/api/eventos/43')
+    const getEvento = (id) => {
+        console.log("id = " + id)
+        fetch(`http://localhost:5000/api/eventos/${id}`)
             .then(async response => {
                 const data = await response.json();
 
@@ -20,56 +21,55 @@ export default () => {
                     return Promise.reject(error);
                 }
                 setEvento(data);
-            })                
+            })
     }
-    {
-        return (
-            <div>
-                ? <h1>{evento.titulo}</h1>                
-                <div name="status">
-                    <h3> Status</h3>
-                    <p>{evento.status}</p>
-                </div>
-                <div name="categoria">
-                    <h3> Categoria</h3>
-                    <p>{evento.categoria}</p>
-                </div>
-                <div name="descricao">
-                    <h3> Descrição</h3>
-                    <p>{evento.descricao}</p>
-                </div>
-                <div name="motivo">
-                    <h3> Motivo</h3>
-                    <p>{evento.motivo}</p>
-                </div>
-                <div name="data">
-                    <h3> Data</h3>
-                    <p>{evento.data}</p>
-                </div>
-                <div name="expectativa">
-                    <h3> Expectativa</h3>
-                    <p>{evento.expectativa}</p>
-                </div>
-                <div name="local">
-                    <h3> local</h3>
-                    <p>{evento.local}</p>
-                </div>
-                <div name="pessoasEnvolvidas">
-                    <h3>Pessoas envolvidas</h3>
-                    <p>{evento.pessoasEnvolvidas}</p>
-                </div>
-                <div name="feedback">
-                    <h3> feedback</h3>
-                    <p>{evento.feedback}</p>
-                </div>
-                <div name="pontosDeMelhoria">
-                    <h3>Pontos de melhoria</h3>
-                    <p>{evento.pontosDeMelhoria}</p>
-                </div>
-                <div name="sentimentos">
-                    <h3>Sentimentos</h3>
-                </div>
+
+    return (
+        <div>
+            ? <h1>{evento.titulo}</h1>
+            <div name="status">
+                <h3> Status</h3>
+                <p>{evento.status}</p>
             </div>
-        );
-    }
+            <div name="categoria">
+                <h3> Categoria</h3>
+                <p>{evento.categoria}</p>
+            </div>
+            <div name="descricao">
+                <h3> Descrição</h3>
+                <p>{evento.descricao}</p>
+            </div>
+            <div name="motivo">
+                <h3> Motivo</h3>
+                <p>{evento.motivo}</p>
+            </div>
+            <div name="data">
+                <h3> Data</h3>
+                <p>{evento.data}</p>
+            </div>
+            <div name="expectativa">
+                <h3> Expectativa</h3>
+                <p>{evento.expectativa}</p>
+            </div>
+            <div name="local">
+                <h3> local</h3>
+                <p>{evento.local}</p>
+            </div>
+            <div name="pessoasEnvolvidas">
+                <h3>Pessoas envolvidas</h3>
+                <p>{evento.pessoasEnvolvidas}</p>
+            </div>
+            <div name="feedback">
+                <h3> feedback</h3>
+                <p>{evento.feedback}</p>
+            </div>
+            <div name="pontosDeMelhoria">
+                <h3>Pontos de melhoria</h3>
+                <p>{evento.pontosDeMelhoria}</p>
+            </div>
+            <div name="sentimentos">
+                <h3>Sentimentos</h3>
+            </div>
+        </div>
+    );
 }
--- src/main/rancoapp/src/view/EventoLista.js
+++ src/main/rancoapp/src/view/EventoLista.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from 'react-dom';
+import { Link } from "react-router-dom";
 
-class EventoLista extends React.Component {
-    constructor(props) {
-        super(props);
+export default () => {
+    useEffect(() => {
+        getEventos();
+    }, [])
+
+    const [eventos, setEventos] = useState([]);
+
+    const getEventos = () => {
+        fetch('http://localhost:5000/api/eventos')
+            .then(async response => {
+                const data = await response.json();
+
+                // check for error response
+                if (!response.ok) {
+                    // get error message from body or default to response statusText
+                    const error = (data && data.message) || response.statusText;
+                    return Promise.reject(error);
+                }
+                setEventos(data);
+            })
     }
 
-    render() {
-        return (
-            <div>
-                <h1>Eventos</h1>                
-            </div>
-        );
-    }
+    return (
+        <div>
+            <h1>Meus eventos</h1>
+            {eventos.map(evento => (
+                <Link to={`/eventos/${evento.id}`}>
+                    <div key={evento.id}>
+                        <h3>{evento.titulo}</h3>
+                    </div>
+                </Link>
+            ))}
+        </div>
+    )
 }
-
-export default EventoLista;
