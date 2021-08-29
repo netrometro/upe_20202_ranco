c86ba67689a361919635a5ea8e64edab4405099d
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-08-29 18:31:27 -0300

    DetalhesEvento page base implemented

--- src/main/rancoapp/src/view/EventoDetalhes.js
+++ src/main/rancoapp/src/view/EventoDetalhes.js
@@ -2,9 +2,56 @@ import React from "react";
 import ReactDOM from 'react-dom';
 
 class EventoDetalhes extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // this.state = { evento: null };
+    }
+
+    async componentDidMount() {        
+        const response = await fetch('http://localhost:5000/api/eventos/43');
+        const evento = await response.json();
+        this.setState({ evento: evento });            
+    }
+
     render() {
         return (
-            <h1>Detalhes de evento</h1>
+            <div>
+                <h1>Evento</h1>
+                <div name="status">
+                    <p> Status</p>
+                </div>
+                <div name="categoria">
+                    <p> Categoria</p>
+                </div>
+                <div name="descricao">
+                    <p> Descrição</p>
+                </div>
+                <div name="motivo">
+                    <p> Motivo</p>
+                </div>
+                <div name="data">
+                    <p> Data</p>
+                </div>
+                <div name="expectativa">
+                    <p> Expectativa</p>
+                </div>
+                <div name="local">
+                    <p> local</p>
+                </div>
+                <div name="pessoasEnvolvidas">
+                    <p>Pessoas envolvidas</p>
+                </div>
+                <div name="feedback">
+                    <p> feedback</p>
+                </div>
+                <div name="pontosDeMelhoria">
+                    <p>Pontos de melhoria</p>
+                </div>
+                <div name="sentimentos">
+                    <p>Sentimentos</p>
+                </div>
+            </div>
         );
     }
 }
