fa4bb6c4381230542f339aa294343b58728f83e5
DahiseDias <dahisedias@gmail.com>
2021-09-01 10:01:24 -0300

    centralizando interface

--- src/main/rancoapp/src/view/Login.js
+++ src/main/rancoapp/src/view/Login.js
@@ -3,25 +3,27 @@ import "../styles/login.css"
 import { Button, Form, FormGroup, Label, Input} from 'reactstrap';
 import ReactDOM from 'react-dom';
 
-class Cadastro extends React.Component {
+class Login extends React.Component {
     render() {
         return (
-            <div className="container_login">
-                <form className="formulario">
-                    <FormGroup >
-                        <legend className="titulo">login</legend>
-                        <label className="label" for="email">Email</label>
-                        <Input type="email" name="email" id="Email" placeholder="escreva seu email"/>
-                    </FormGroup>
-                    <FormGroup >
-                        <label className="label" for="password">Senha</label>
-                        <Input type="password" name="password" id="Password" placeholder="escreva sua senha"/>
-                    </FormGroup>
-                </form>
-                <Button className="botao" size="lg" > Login </Button>{' '}
-            </div>           
+            <div className='container'>
+                <div className="container_login">
+                    <Form className="formulario">
+                        <FormGroup >
+                            <legend className="titulo">login</legend>
+                            <label className="label" for="email">Email</label>
+                            <Input type="email" name="email" id="Email" placeholder="escreva seu email"/>
+                        </FormGroup>
+                        <FormGroup >
+                            <label className="label" for="password">Senha</label>
+                            <Input type="password" name="password" id="Password" placeholder="escreva sua senha"/>
+                        </FormGroup>
+                    </Form>
+                    <Button className="botao" size="lg" > Login </Button>{' '}
+                </div>           
+            </div>
         );
     };
 };
 
-export default Cadastro;
+export default Login;
