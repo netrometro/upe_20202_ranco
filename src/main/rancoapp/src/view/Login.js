2205ef1db3d2ab96be4f3653a0b367e72441fa11
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-09-01 10:36:40 -0300

    Fixing reactstrap bug in login

--- src/main/rancoapp/src/view/Login.js
+++ src/main/rancoapp/src/view/Login.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../styles/login.css"
-import { Button, Form, FormGroup, Label, Input} from 'reactstrap';
 import ReactDOM from 'react-dom';
 
 class Login extends React.Component {
@@ -8,18 +7,18 @@ class Login extends React.Component {
         return (
             <div className='container'>
                 <div className="container_login">
-                    <Form className="formulario">
-                        <FormGroup >
+                    <form className="formulario">
+                        <div >
                             <legend className="titulo">login</legend>
-                            <label className="label" for="email">Email</label>
-                            <Input type="email" name="email" id="Email" placeholder="escreva seu email"/>
-                        </FormGroup>
-                        <FormGroup >
-                            <label className="label" for="password">Senha</label>
-                            <Input type="password" name="password" id="Password" placeholder="escreva sua senha"/>
-                        </FormGroup>
-                    </Form>
-                    <Button className="botao" size="lg" > Login </Button>{' '}
+                            <label className="label">Email</label>
+                            <input type="email" name="email" id="Email" placeholder="escreva seu email"/>
+                        </div>
+                        <div >
+                            <label className="label">Senha</label>
+                            <input type="password" name="password" id="Password" placeholder="escreva sua senha"/>
+                        </div>
+                    </form>
+                    <button className="botao" size="lg" > Login </button>{' '}
                 </div>           
             </div>
         );
