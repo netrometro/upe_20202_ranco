229215e89ee94e5014d17979dca4c1c06b4cb6a8
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-09-05 11:20:44 -0300

    Removed unneded code

--- src/main/rancoapp/src/view/Login.js
+++ src/main/rancoapp/src/view/Login.js
@@ -7,10 +7,8 @@ import { AuthReducer, initialState } from "../context/reducer";
 
 function Login() {
 
-    const [user, setUser] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [errorText, setErrorText] = useState("");
 
     const dispatch = useAuthDispatch()
     const { loading, errorMessage } = useAuthState()
