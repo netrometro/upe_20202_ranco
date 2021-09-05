cc67f962cfa64ecbb396903c6c4edbc68990187b
DahiseDias <dahisedias@gmail.com>
2021-09-05 19:11:21 -0300

    grafico de pizza

--- src/main/rancoapp/src/view/Dashboard.js
+++ src/main/rancoapp/src/view/Dashboard.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import "../styles/lista.css"
 import { Link } from "react-router-dom";
 import { useAuthDispatch, useAuthState } from "../context";
+import { Pie } from 'react-chartjs-2'
+
 
 export default () => {
     useEffect(() => {
@@ -11,10 +13,12 @@ export default () => {
     const state = useAuthState();
     const dispatch = useAuthDispatch()
 
-    const [eventos, setEventos] = useState([]);
+    const [status, setStatus] = useState([]);
+    const [categoria, setCategoria] = useState([]);
+    const [motivo, setMotivo] = useState([]);
 
     const getEventos = () => {
-        fetch('http://localhost:5000/api/eventos')
+        fetch('http://localhost:5000/dados/52')
             .then(async response => {
                 const data = await response.json();
 
@@ -25,32 +29,65 @@ export default () => {
                     return Promise.reject(error);
                 }
                 console.log(data);
-                setEventos(data);
+                setStatus(data.status);
+                setCategoria(data.categoria);
+                setMotivo(data.motivo);
             })
     }
+
+    
 
     if (state.userDetails) {
 
         return (
             <div>
-                {/* <div className='listaEvento'>
-                    <h1 id='eventos'>Dashboard</h1>
-                    <a href='/adicionarEvento'>Adicionar Evento</a>
-                    {eventos.map((evento) => (
-                        <Link to={`/eventos/${evento.id}`} key={evento.id}>
-                            <div className='blocoEvento'>
-                                <h3>{evento.titulo}</h3>
-                                <h3>Categoria : {evento.categoria}</h3>
-                                {/* se o status for false bloco vermelho, se for true, verde */}
-                          {/*   </div>
-                        </Link>
-                    ))}
+                <div className='listaEvento'>
+                <Pie
+        data={{
+          labels: ['Status', 'Categoria', 'Motivo'],
+          datasets: [
+            {
+              label: '# of votes',
+              data: [12, 19, 3,],
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
 
-                </div> */} */}
+              ],
+              borderWidth: 1,
+            },
 
-                <canvas>
-                    
-                </canvas>
+          ],
+        }}
+        height={400}
+        width={600}
+        options={{
+          maintainAspectRatio: false,
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            ],
+          },
+          legend: {
+            labels: {
+              fontSize: 25,
+            },
+          },
+        }}
+      />
+    </div>
+
+               
 
             </div>
         )
