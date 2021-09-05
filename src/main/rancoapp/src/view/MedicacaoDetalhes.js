b62fae9418f15ce6b7d80c6dab301b7e4ef3a259
Clara626 <clara.v.araujo626@gmail.com>
2021-09-05 14:41:02 -0300

    Organização de  Colunas

--- src/main/rancoapp/src/view/MedicacaoDetalhes.js
+++ src/main/rancoapp/src/view/MedicacaoDetalhes.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from 'react-dom';
 
-export default ({match}) => {
-    useEffect(() => {        
+export default ({ match }) => {
+    useEffect(() => {
         getMedicacao(match.params.id);
     }, [])
 
@@ -23,43 +23,43 @@ export default ({match}) => {
             })
     }
     return (
-        <div className = 'listaMedicamento'>
-            <div className = 'header' id = 'header'>
-                <h1>{medicacoes.nome}</h1>
-                <div className = 'colunas' id = 'colunas'>
-            <div className = 'primeiraColuna'>
-            <div className= 'blocoMedicamento' name="descricao" id="descricao">
-                <h3>Descricao</h3>
-                <p>{medicacoes.descricao}</p>
-            </div>
-            <div className= 'blocoMedicamento' name="observacao" id="observacao">
-                <h3>Observação</h3>
-                <p>{medicacoes.observacao}</p>
-            </div>
-            </div>
+        <div className='listaMedicamento'>
+            <h1>{medicacoes.nome}</h1>
+            <div className = ''>
+                <div className='colunas' id='colunas'>
+                    <div className='primeiraColuna'>
+                        <div className='blocoMedicamento' name="descricao" id="descricao">
+                            <h3>Descricao</h3>
+                            <p>{medicacoes.descricao}</p>
+                        </div>
+                        <div className='blocoMedicamento' name="observacao" id="observacao">
+                            <h3>Observação</h3>
+                            <p>{medicacoes.observacao}</p>
+                        </div>
+                    </div>
 
-            <div className = 'segundaColuna'>
-            <div className= 'blocoMedicamento' name="intervalo" id="intervalo">
-                <h3>Intervalo</h3>
-                <p>{medicacoes.intervalo}</p>
-            </div>
-            <div className= 'blocoMedicamento' name="ultimaDosagem" id="ultimaDosagem">
-                <h3>Última Dosagem</h3>
-                <p>{medicacoes.ultimaDosagem}</p>
-            </div>
-            <div className= 'blocoMedicamento' name="posologia" id="posologia">
-                <h3> Posologia</h3>
-                <p>{medicacoes.posologia}</p>
-            </div>
-            </div>
+                    <div className='segundaColuna'>
+                        <div className='blocoMedicamento' name="intervalo" id="intervalo">
+                            <h3>Intervalo</h3>
+                            <p>{medicacoes.intervalo}</p>
+                        </div>
+                        <div className='blocoMedicamento' name="ultimaDosagem" id="ultimaDosagem">
+                            <h3>Última Dosagem</h3>
+                            <p>{medicacoes.ultimaDosagem}</p>
+                        </div>
+                        <div className='blocoMedicamento' name="posologia" id="posologia">
+                            <h3> Posologia</h3>
+                            <p>{medicacoes.posologia}</p>
+                        </div>
+                    </div>
+
+                </div>
+
+
+
+
 
             </div>
-            </div>
-
-            
-            
-            
-            
         </div>
     );
 }
