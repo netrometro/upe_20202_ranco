a5dccb853338439446b1bafba1e118c6d11e4531
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-09-05 14:14:04 -0300

    block navigation if user is not loged

--- src/main/rancoapp/src/components/navigation.jsx
+++ src/main/rancoapp/src/components/navigation.jsx
@@ -1,4 +1,14 @@
+import { logout, useAuthDispatch, useAuthState } from "../context";
+
 export const Navigation = (props) => {
+  const state = useAuthState();
+  const dispatch = useAuthDispatch()
+  console.log(state)
+
+  const handleLogout = () => {
+    logout(dispatch)
+    window.location.href = '/'
+  }
   return (
     <nav className='navbar'>
       <div className='navegacao'>
@@ -9,32 +19,66 @@ export const Navigation = (props) => {
         </div>
 
         <div className='paginas'>
-            <li>
-              <a href='/eventos' className='page'>
-                Eventos
-              </a>
-            </li>
-            <li>
-              <a href='/medicacoes' className='page'>
-                Medicações
-              </a>
-            </li>
-            <li>
-              <a href='/cadastrarmedicacoes' className='page'>
-                Novas Medicações
-              </a>
-            </li>
-            <li>
-              <a href='/cadastro' className='page'>
-                Cadastro
-              </a>
-            </li>
-            <li>
-              <a href='/perfil' className='page'>
-                Perfil
-              </a>
-            </li>
-            
+          {
+            state.userDetails
+              ?
+              <li>
+                <a href='/eventos' className='page'>
+                  Eventos
+                </a>
+              </li>
+              : <></>
+          }
+          {
+            state.userDetails
+              ?
+              <li>
+                <a href='/medicacoes' className='page'>
+                  Medicações
+                </a>
+              </li>
+
+              : <></>
+          }
+
+          {
+            state.userDetails
+              ?
+              <li>
+                <a href='/cadastrarmedicacoes' className='page'>
+                  Novas Medicações
+                </a>
+              </li>
+
+              : <></>
+          }
+          <li>
+            <a href='/cadastro' className='page'>
+              Cadastro
+            </a>
+          </li>
+          {
+            state.userDetails
+              ?
+              <li>
+                <a href='/perfil' className='page'>
+                  Perfil
+                </a>
+              </li>
+
+              : <></>
+          }
+          {
+            state.userDetails
+              ?
+              <li>
+                <button onClick={handleLogout} className='page'>
+                  sair
+                </button>
+              </li>
+
+              : <></>
+          }
         </div>
       </div>
     </nav>
