0e19c24640e9fa8b2232bb112fd2c443d2bd3406
DahiseDias <dahisedias@gmail.com>
2021-09-04 23:34:53 -0300

    adicionando diferentes inputs

--- src/main/rancoapp/src/view/adicionarEvento.js
+++ src/main/rancoapp/src/view/adicionarEvento.js
@@ -6,9 +6,23 @@ export default ({match}) => {
     return (
         <div className='listaEvento' >
             <div className= 'header'>
-            <h1>titulo</h1>
+            <textarea rows="1" cols="80"  id="titulo" placeholder= 'Titulo'></textarea>
                 <div className='blocoEvento' name="status" id='status'>
                     <p>status</p>
+                   {/*  <div tag="fieldset">
+                        <div >
+                            <label className="texto">
+                            <input  type="radio" name="tipo"/>{' '}
+                            Aberto
+                            </label>
+                        </div>
+                        <div >
+                            <label className="texto">
+                            <input  type="radio" name="tipo"/>{' '}
+                            Fechado
+                            </label>
+                        </div>
+                    </div> */}
                 </div>
             </div>
             <div className='colunas'>
@@ -33,14 +47,86 @@ export default ({match}) => {
                 <div className='coluna2'>
                     <div id='detalhes'>
                     <div  className='blocoEvento' name="categoria">
-                        <h3> Categoria</h3> //
+                        <h3> Categoria</h3> 
+                        <div tag="fieldset">
+                                <div >
+                                    <label className="texto">
+                                    <input  type="radio" name="tipo"/>{' '}
+                                    Amigos
+                                    </label>
+                                </div>
+                                <div >
+                                    <label className="texto">
+                                    <input  type="radio" name="tipo"/>{' '}
+                                    Desconhecidos
+                                    </label>
+                                </div>
+                                <div >
+                                    <label className="texto">
+                                    <input  type="radio" name="tipo"/>{' '}
+                                    Escola
+                                    </label>
+                                </div>
+                                <div >
+                                    <label className="texto">
+                                    <input  type="radio" name="tipo"/>{' '}
+                                    Faculdade
+                                    </label>
+                                </div>
+                                <div>
+                                    <label className="texto">
+                                    <input type="radio" name="tipo"/>{' '}
+                                    Familia
+                                    </label>
+                                </div>
+                                <div >
+                                    <label className="texto">
+                                    <input  type="radio" name="tipo"/>{' '}
+                                    Trabalho
+                                    </label>
+                                </div>
+                            </div>
                     </div>
                     
                     <div className='blocoEvento' name="motivo">
-                        <h3> Motivo</h3> //
+                        <h3> Motivo</h3> 
+                        <div tag="fieldset">
+                                <div >
+                                    <label className="texto">
+                                    <input  type="radio" name="tipo"/>{' '}
+                                    Culpa
+                                    </label>
+                                </div>
+                                <div >
+                                    <label className="texto">
+                                    <input  type="radio" name="tipo"/>{' '}
+                                    Medo
+                                    </label>
+                                </div>
+                                <div >
+                                    <label className="texto">
+                                    <input  type="radio" name="tipo"/>{' '}
+                                    Timidez
+                                    </label>
+                                </div>
+                                <div >
+                                    <label className="texto">
+                                    <input  type="radio" name="tipo"/>{' '}
+                                    Tristeza
+                                    </label>
+                                </div>
+                                <div>
+                                    <label className="texto">
+                                    <input type="radio" name="tipo"/>{' '}
+                                    Vergonha
+                                    </label>
+                                </div>
+                            </div>
                     </div>
                     <div className='blocoEvento' name="data">
                         <h3> Data</h3>
+                        <input type="date" id="data" name="data" placeholder="Data">
+                        </input>
                     </div>
                     
                     <div className='blocoEvento' name="local">
