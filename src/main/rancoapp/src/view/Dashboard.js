f41018677ce941f77aa2ce8fc21f82ead8e62b6e
DahiseDias <dahisedias@gmail.com>
2021-09-06 22:50:36 -0300

    adicionando listas ao dashboard

--- src/main/rancoapp/src/view/Dashboard.js
+++ src/main/rancoapp/src/view/Dashboard.js
@@ -1,39 +1,75 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from 'react-dom';
-import "../styles/lista.css"
+import "../styles/dashboard.css"
 import { Link } from "react-router-dom";
 import { useAuthDispatch, useAuthState } from "../context";
 import { Pie } from 'react-chartjs-2'
 
 
 export default () => {
-  useEffect(() => {
-    getEventos();
-  }, [])
-  const state = useAuthState();
-  const dispatch = useAuthDispatch()
+    useEffect(() => {
+        getEventos();
+        getDados();
+        getMedicacao();
+    }, [])
+    const state = useAuthState();
+    const dispatch = useAuthDispatch()
 
-  const [status, setStatus] = useState([]);
-  const [categoria, setCategoria] = useState([]);
-  const [motivo, setMotivo] = useState([]);
+    const [status, setStatus] = useState([]);
+    const [categoria, setCategoria] = useState([]);
+    const [motivo, setMotivo] = useState([]);
+    const [eventos, setEventos] = useState([]);
+    const [medicacoes, setMedicacao] = useState([]);
+    const getMedicacao = () => {
+        fetch('http://localhost:5000/api/medicacoes')
+            .then(async response => {
+                const data = await response.json();
 
-  const getEventos = () => {
-    fetch('http://localhost:5000/dados/' + JSON.parse(state.userDetails).id)
-      .then(async response => {
-        const data = await response.json();
+                // check for error response
+                if (!response.ok) {
+                    // get error message from body or default to response statusText
+                    const error = (data && data.message) || response.statusText;
+                    return Promise.reject(error);
+                }
+                console.log(data);
+                setMedicacao(data);
+            })
+    }
 
-        // check for error response
-        if (!response.ok) {
-          // get error message from body or default to response statusText
-          const error = (data && data.message) || response.statusText;
-          return Promise.reject(error);
-        }
-        console.log(data);
-        setStatus(data.status);
-        setCategoria(data.categoria);
-        setMotivo(data.motivo);
-      })
-  }
+    const getEventos = () => {
+        fetch(`http://localhost:5000/eventos/${JSON.parse(state.userDetails).id}`)
+            .then(async response => {
+                const data = await response.json();
+                // check for error response
+                if (!response.ok) {
+                    // get error message from body or default to response statusText
+                    const error = (data && data.message) || response.statusText;
+                    return Promise.reject(error);
+                }
+                console.log(data);
+                setEventos(data.eventos);
+            })
+    }
+
+    const getDados = () => {
+        fetch('http://localhost:5000/dados/' + JSON.parse(state.userDetails).id)
+            .then(async response => {
+                const data = await response.json();
+
+                // check for error response
+                if (!response.ok) {
+                    // get error message from body or default to response statusText
+                    const error = (data && data.message) || response.statusText;
+                    return Promise.reject(error);
+                }
+                console.log(data);
+                setStatus(data.status);
+                setCategoria(data.categoria);
+                setMotivo(data.motivo);
+            })
+    }
+
+
 
     /* categoria */
     var qtdTrabalho = categoria.filter(x => x === 'TRABALHO').length
@@ -56,159 +92,190 @@ export default () => {
 
 
 
-  if (state.userDetails) {
+    if (state.userDetails) {
         return (
             <div>
                 <div className='listaEvento'>
-                    <Pie
-                        data={{
-                            labels: ['Trabalho', 'Amigos', 'Desconhecidos', 'Escola', 'Faculdade', 'Familia'],
-                            datasets: [
-                                {
-                                    label: 'Categorias',
-                                    data: [qtdTrabalho, qtdAmigos, qtdDesconhecidos, qtdEscola, qtdFaculdade, qtdFamilia],
-                                    backgroundColor: [
-                                        'rgba(255, 99, 132, 0.2)',
-                                        'rgba(54, 162, 235, 0.2)',
-                                        'rgba(255, 206, 86, 0.2)',
-                                        'rgba(75, 192, 192, 0.2)',
-                                        'rgba(153, 102, 255, 0.2)',
-                                        'rgba(255, 159, 64, 0.2)',
-                                    ],
-                                    borderColor: [
-                                        'rgba(255, 99, 132, 1)',
-                                        'rgba(54, 162, 235, 1)',
-                                        'rgba(255, 206, 86, 1)',
-                                        'rgba(75, 192, 192, 1)',
-                                        'rgba(153, 102, 255, 1)',
-                                        'rgba(255, 159, 64, 1)',
+                    <div className='header'>
+                        <h1>DashBoard</h1>
+                    </div>
+                    <div className='graficos'>
+                        <div className='grafico' >
+
+                            <Pie
+                                data={{
+                                    labels: ['Trabalho', 'Amigos', 'Desconhecidos', 'Escola', 'Faculdade', 'Familia'],
+                                    datasets: [
+                                        {
+                                            label: 'Categorias',
+                                            data: [qtdTrabalho, qtdAmigos, qtdDesconhecidos, qtdEscola, qtdFaculdade, qtdFamilia],
+                                            backgroundColor: [
+                                                'rgba(255, 99, 132, 1)',
+                                                'rgba(54, 162, 235, 1)',
+                                                'rgba(255, 206, 86, 1)',
+                                                'rgba(75, 192, 192, 1)',
+                                                'rgba(153, 102, 255, 1)',
+                                                'rgba(255, 159, 64, 1)',
+                                            ],
+                                            borderColor: [
+                                                'rgba(255, 255, 255, 1)',
+                                            ],
+                                            borderWidth: 1,
+                                        },
 
                                     ],
-                                    borderWidth: 1,
-                                },
-
-                            ],
-                        }}
-                        height={100}
-                        width={200}
-                        options={{
-                            maintainAspectRatio: true,
-                            scales: {
-                                yAxes: [
-                                    {
-                                        ticks: {
-                                            beginAtZero: true,
+                                }}
+                                height={200}
+                                width={300}
+                                options={{
+                                    maintainAspectRatio: false,
+                                    scales: {
+                                        yAxes: [
+                                            {
+                                                ticks: {
+                                                    beginAtZero: true,
+                                                },
+                                            },
+                                        ],
+                                    },
+                                    legend: {
+                                        labels: {
+                                            fontSize: 15,
                                         },
                                     },
-                                ],
-                            },
-                            legend: {
-                                labels: {
-                                    fontSize: 15,
-                                },
-                            },
-                        }}
-                    />
-                    <Pie
-                        data={{
-                            labels: ['Ativos', 'Fechados'],
-                            datasets: [
-                                {
-                                    label: 'Status',
-                                    data: [qtdTrue, qtdFalse],
-                                    backgroundColor: [
-                                        'rgba(255, 99, 132, 0.2)',
-                                        'rgba(54, 162, 235, 0.2)',
-                                        'rgba(255, 206, 86, 0.2)',
-                                        'rgba(75, 192, 192, 0.2)',
-                                        'rgba(153, 102, 255, 0.2)',
-                                        'rgba(255, 159, 64, 0.2)',
-                                    ],
-                                    borderColor: [
-                                        'rgba(255, 99, 132, 1)',
-                                        'rgba(54, 162, 235, 1)',
-                                        'rgba(255, 206, 86, 1)',
-                                        'rgba(75, 192, 192, 1)',
-                                        'rgba(153, 102, 255, 1)',
-                                        'rgba(255, 159, 64, 1)',
+                                }}
+                            />
+                        </div>
+
+                        <div className='grafico'>
+                            <Pie
+                                data={{
+                                    labels: ['Ativos', 'Fechados'],
+                                    datasets: [
+                                        {
+                                            label: 'Status',
+                                            data: [qtdTrue, qtdFalse],
+                                            backgroundColor: [
+                                                'rgba(255, 99, 132, 1)',
+                                                'rgba(54, 162, 235, 1)',
+                                                'rgba(255, 206, 86, 1)',
+                                                'rgba(75, 192, 192, 1)',
+                                                'rgba(153, 102, 255, 1)',
+                                                'rgba(255, 159, 64, 1)',
+                                            ],
+                                            borderColor: [
+                                                'rgba(255, 255, 255, 1)',
+
+                                            ],
+                                            borderWidth: 1,
+                                        },
 
                                     ],
-                                    borderWidth: 1,
-                                },
-
-                            ],
-                        }}
-                        height={200}
-                        width={300}
-                        options={{
-                            maintainAspectRatio: true,
-                            scales: {
-                                yAxes: [
-                                    {
-                                        ticks: {
-                                            beginAtZero: true,
+                                }}
+                                height={200}
+                                width={300}
+                                options={{
+                                    maintainAspectRatio: false,
+                                    scales: {
+                                        yAxes: [
+                                            {
+                                                ticks: {
+                                                    beginAtZero: true,
+                                                },
+                                            },
+                                        ],
+                                    },
+                                    legend: {
+                                        labels: {
+                                            fontSize: 15,
                                         },
                                     },
-                                ],
-                            },
-                            legend: {
-                                labels: {
-                                    fontSize: 15,
-                                },
-                            },
-                        }}
-                    />
-                    <Pie
-                        data={{
-                            labels: ['Culpa', 'Medo', 'Timidez', 'Tristeza', 'Vergonha'],
-                            datasets: [
-                                {
-                                    label: 'Motivos',
-                                    data: [qtdCulpa, qtdMedo, qtdTimidez, qtdTristeza, qtdVergonha],
-                                    backgroundColor: [
-                                        'rgba(255, 99, 132, 0.2)',
-                                        'rgba(54, 162, 235, 0.2)',
-                                        'rgba(255, 206, 86, 0.2)',
-                                        'rgba(75, 192, 192, 0.2)',
-                                        'rgba(153, 102, 255, 0.2)',
-                                        'rgba(255, 159, 64, 0.2)',
-                                    ],
-                                    borderColor: [
-                                        'rgba(255, 99, 132, 1)',
-                                        'rgba(54, 162, 235, 1)',
-                                        'rgba(255, 206, 86, 1)',
-                                        'rgba(75, 192, 192, 1)',
-                                        'rgba(153, 102, 255, 1)',
-                                        'rgba(255, 159, 64, 1)',
+                                }}
+                            />
+                        </div>
+
+                        <div className='grafico'>
+
+                            <Pie
+                                data={{
+                                    labels: ['Culpa', 'Medo', 'Timidez', 'Tristeza', 'Vergonha'],
+                                    datasets: [
+                                        {
+                                            label: 'Motivos',
+                                            data: [qtdCulpa, qtdMedo, qtdTimidez, qtdTristeza, qtdVergonha],
+                                            backgroundColor: [
+                                                'rgba(255, 99, 132, 1)',
+                                                'rgba(54, 162, 235, 1)',
+                                                'rgba(255, 206, 86, 1)',
+                                                'rgba(75, 192, 192, 1)',
+                                                'rgba(153, 102, 255, 1)',
+                                                'rgba(255, 159, 64, 1)',
+                                            ],
+                                            borderColor: [
+                                                'rgba(255, 255, 255, 1)',
+
+                                            ],
+                                            borderWidth: 1,
+                                        },
 
                                     ],
-                                    borderWidth: 1,
-                                },
-
-                            ],
-                        }}
-                        height={200}
-                        width={300}
-                        options={{
-                            maintainAspectRatio: true,
-                            scales: {
-                                yAxes: [
-                                    {
-                                        ticks: {
-                                            beginAtZero: true,
+                                }}
+                                height={200}
+                                width={300}
+                                options={{
+                                    maintainAspectRatio: false,
+                                    scales: {
+                                        yAxes: [
+                                            {
+                                                ticks: {
+                                                    beginAtZero: true,
+                                                },
+                                            },
+                                        ],
+                                    },
+                                    legend: {
+                                        labels: {
+                                            fontSize: 15,
                                         },
                                     },
-                                ],
-                            },
-                            legend: {
-                                labels: {
-                                    fontSize: 15,
-                                },
-                            },
-                        }}
-                    />
+                                }}
+                            />
+
+                        </div>
+                    </div>
+                    <div id='listas'>
+
+                  
+                    <div className='listaEvento' id='dashboard'>
+                        <div className='header'>
+                            <h1 id='eventos'>Meus Eventos</h1>
+                        </div>
+                        {eventos.map((evento, i) => (
+                            <Link to={`/eventos/${evento.id}`} key={evento.id}>
+                                <div className='blocoEvento'>
+                                    <h3>{evento.titulo}</h3>
+                                    <div className='blocoEvento' name="status" id={evento.status ? 'statusTRUE' : 'statusFALSE'}>
+                                    </div>
+                                    <h3>Categoria : {evento.categoria}</h3>
+                                    {/* se o status for false bloco vermelho, se for true, verde */}
+                                </div>
+                            </Link>
+                        ))}
+                    </div>
+                    <div className="listaEvento" id='dashboard'>
+                        <h1 id='medicacao'>Minhas Medicações</h1>
+                        {medicacoes.map((medicacao) => (
+                            <Link to={`/medicacoes/${medicacao.id}`} key={medicacao.id}>
+                                <div className="blocoEvento">
+                                    <h3>{medicacao.nome}</h3>
+                                    <h3>Posologia: {medicacao.posologia}</h3>
+                                </div>
+                            </Link>
+                        ))}
+                    </div>
+                    </div>
+
                 </div>
-
 
             </div>
         )
